356921852f26f7daddac90affa1cde49128f7d41
Matthew Riedel <mattr@orionhealth.com>
2018-04-24 09:41:01 +1200

    fixing formatting

--- {{cookiecutter.app_name}}/cmd/root.go
+++ {{cookiecutter.app_name}}/cmd/root.go
@@ -73,7 +73,7 @@ func initConfig() {
 	// If a config file is found, read it in.
 	if err := viper.ReadInConfig(); err == nil {
 		fmt.Println("Using config file:", viper.ConfigFileUsed())
-	}{% else %}
+	}{% else -%}
 func init() {
     // Cobra also supports local flags, which will only run
 	// when this action is called directly.
